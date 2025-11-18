66e0b99a6579d3259148b11e84e405ec06ff8894
AbhinavPraveenKumar <praveenkuar@richlandrams.net>
2025-11-18 16:24:58 -0500

    Added tileset collisions, jumping, and sprinting

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var SPEED = 300.0
+var JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -21,5 +21,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var sprint := Input.is_action_pressed("sprint")
+	if sprint:
+		SPEED = 450
+	else:
+		SPEED = 300
+	
 	move_and_slide()
