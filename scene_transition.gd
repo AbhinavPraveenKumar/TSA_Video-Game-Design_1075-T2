0278c4241e2af33efdc49060d18bed43c47068b8
Abhinav Praveen Kumar <PraveenKUAR@richlandrams.net>
2025-11-25 10:36:14 -0500

    added get_tree to fix bug (still doesn't work)(so basically nothing v3) (nothing v2.1 as a compromise)

--- scene_transition.gd
+++ scene_transition.gd
@@ -9,5 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if body_entered and is_in_group("character"):
-		SceneTree.change_scene_to_file("res://level_1_2.tscn")
+		get_tree().change_scene_to_file("res://level_1_2.tscn")
 	pass
