0017a4f85b8a27e25e1cc0419dd7b50a48957bfa
AbhinavPraveenKumar <praveenkuar@richlandrams.net>
2025-11-25 16:25:26 -0500

    added invisible wall at beginning of level 1 scene 1 to stop player from falling off the map

--- scene_transition.gd
+++ scene_transition.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var scene_number=1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,7 +8,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if body_entered and is_in_group("character"):
 		get_tree().change_scene_to_file("res://level_1_2.tscn")
 	pass
