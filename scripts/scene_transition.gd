a8bd5669249304d83c78d69d32efbf2c6a7ed951
Abhinav Praveen Kumar <PraveenKUAR@richlandrams.net>
2026-01-07 10:40:09 -0500

    expanded scene_trans function, will make area_entered signal checker function (possibly incorporated into scene_trans?)

--- scripts/scene_transition.gd
+++ scripts/scene_transition.gd
@@ -1,34 +1,37 @@
 extends Area2D
 
-var level_num=1;
+var level_num=1; # move this to a different script when we add a second level
 var scene_num=1;
+
 func scene_trans(forward_bool):
-	if forward_bool==true:
-		if level_num==1:
-			if scene_num==1:
-				get_tree().change_scene_to_file("res://level_1-1/level_1_2.tscn")
-				scene_num=2;
-			elif scene_num==2:
-				get_tree().change_scene_to_file("res://level_1-1/level_1_3.tscn")
-				scene_num=3;
+	# print("scene_trans function running") # Debug
+	if area_entered and is_in_group("character"):
+		if forward_bool==true:
+			if level_num==1:
+				if scene_num==1:
+					get_tree().change_scene_to_file("res://level_1-1/level_1_2.tscn")
+					scene_num=2;
+				elif scene_num==2:
+					get_tree().change_scene_to_file("res://level_1-1/level_1_3.tscn")
+					scene_num=3;
 		elif forward_bool==false:
-			if scene_num==2:
-				get_tree().change_scene_to_file("res://level_1-1/level_1_1.tscn")
-				scene_num=1;
-			elif scene_num==3:
-				get_tree().change_scene_to_file("res://level_1-1/level_1_2.tscn")
-				scene_num=2;
-	pass
+			if scene_num==1:
+				if scene_num==2:
+					get_tree().change_scene_to_file("res://level_1-1/level_1_1.tscn")
+					scene_num=1;
+				elif scene_num==3:
+					get_tree().change_scene_to_file("res://level_1-1/level_1_2.tscn")
+					scene_num=2;
+			print("Scene transition successful") # Debug
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	scene_trans(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # activates scene transition function
 func _process(_delta: float) -> void:
 	# print('working')
-	if body_entered and is_in_group("character"):
-		scene_trans(true);
+	scene_trans(true);
 	pass
