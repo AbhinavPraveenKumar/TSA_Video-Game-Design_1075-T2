7229dadd5ebaa99ff4aa169601a506d71a403c00
Abhinav Praveen Kumar <PraveenKUAR@richlandrams.net>
2026-01-08 11:06:38 -0500

    SCENE TRANSITIONS WORK!!! YEEEEEEEEEEEEEEEES! (note that it only works for first scene, seems to be some variable issue) Also fixed debug message issue.

--- scripts/scene_transition.gd
+++ scripts/scene_transition.gd
@@ -4,7 +4,7 @@ var level_num=1; # move this to a different script when we add a second level
 var scene_num=1;
 
 func scene_trans(forward_bool):
-	# print("scene_trans function running") # Debug
+		print("scene_trans function running") # Debug
 		if forward_bool==true:
 			if level_num==1:
 				if scene_num==1:
@@ -14,18 +14,18 @@ func scene_trans(forward_bool):
 					get_tree().change_scene_to_file("res://level_1-1/level_1_3.tscn")
 					scene_num=3;
 		elif forward_bool==false:
-			if scene_num==1:
+			if level_num==1:
 				if scene_num==2:
 					get_tree().change_scene_to_file("res://level_1-1/level_1_1.tscn")
 					scene_num=1;
 				elif scene_num==3:
 					get_tree().change_scene_to_file("res://level_1-1/level_1_2.tscn")
 					scene_num=2;
-			print("Scene transition successful") # Debug
+		print("Scene transition successful") # Debug
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	scene_trans(true)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,3 +33,9 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	# print('working')
 	pass
+	
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		scene_trans(true)
