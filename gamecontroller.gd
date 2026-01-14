1e118eb63d8ebde95523f80f56811e2afe27871f
AbhinavPK0 <praveenkuar@richlandrams.net>
2026-01-14 06:52:42 -0500

    Added new GameController node and script.

---
+++ gamecontroller.gd
@@ -0,0 +1,12 @@
+class_name GameController extends Node
+
+var gamecontroller: GameController
+@export var World_2D: Node2D
+@export var GUI: Control
+var current_2D_scene
+var current_GUI_scenet
+func _ready() -> void:
+	pass #Global.game_controller=self
+	
+func _process(delta: float) -> void:
+	pass
